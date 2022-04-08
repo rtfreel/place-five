b755e3dcff50b94953acf59cde0f5eb55bd984fd
Tkachenko <roman.tkachenko@sigmaukraine.com>
2022-04-08 18:48:22 +0300

    made game field resizable

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,22 @@
-class Game{
+const MAX_SIZE = 15,
+      MIN_SIZE = 5,
+      SIZE_FONT_REL = {
+          5: 10,
+          6: 8.4,
+          7: 7.1,
+          8: 6.1,
+          9: 5.4,
+          10: 4.8,
+          11: 4.3,
+          12: 3.9,
+          13: 3.6,
+          14: 3.3,
+          15: 3
+      };
 
+class Game{
     constructor(size) {
         this.size = size;
-        this.table = []
-
         this.generateField();
     }
 
@@ -17,6 +30,14 @@ class Game{
             $("#game-field").append(cur);
         }
         this.roundCorners();
+        $(".square").css("font-size", SIZE_FONT_REL[this.size] + "vh");
+        $("#size-label").html(this.size + "&times;" + this.size);
+
+        $(".square").click(function(){
+            const row = $(this).parent().index();
+            const col = $(this).index();
+            mark(row, col);
+         });
     }
 
     roundCorners() {
--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,25 @@ let game;
 let tic = true;
 
 $(document).ready(function(){
-    game = new Game(10);
-
-    $(".square").click(function(){
-       const row = $(this).parent().index();
-       const col = $(this).index();
-       mark(row, col);
-    });
+    game = new Game(MIN_SIZE);
 });
 
 function mark(row, col){
     let sym = tic ? "&times;" : "o";
     $("#game-field tr:eq(" + row + ") td:eq(" + col + ")").html(sym);
     tic = !tic;
+}
+
+function resize(add) {
+    let newSize;
+    if(add) {   // make table larger by one
+        if(game.size == MAX_SIZE)
+            return;
+        newSize = game.size + 1;
+    } else {    // shrink table by one
+        if(game.size == MIN_SIZE)
+            return;
+        newSize = game.size - 1;
+    }
+    game = new Game(newSize);
 }
